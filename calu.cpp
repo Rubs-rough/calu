e9ff4f17568f83ec03bab265d0d4983616a7d6a3
Rubs-rough <13011133266@163.com>
2016-01-04 22:33:02 -0500

    --debug successfully

--- calu.cpp
+++ calu.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int getPriority(char operators)  
 {  
-    if(operators == '(' )  
+    if(operators == '(')  
     {  
         return 0;  
     }  
@@ -15,14 +15,15 @@ int getPriority(char operators)
     else if(operators == '*' || operators == '/')  
     {  
         return 2;  
-    }  
+    }
     else if(operators == ')')
     {
 	return 3;
     }
 }  
 
-inline int compare(string str1, string str2) {
+inline int compare(string str1, string str2) 
+{
     if(str1.size() > str2.size())
 	return 1;
     else if(str1.size() < str2.size())
@@ -30,33 +31,45 @@ inline int compare(string str1, string str2) {
     else
         return str1.compare(str2);
 }
-string ADD_INT(string str1, string str2) {
+string ADD_INT(string str1, string str2)
+{
     string SUB_INT(string str1, string str2);
     int sign = 1;
     string str;
-    if(str1[0] == '-') {
-        if(str2[0] == '-') {
+    if(str1[0] == '-') 
+    {
+        if(str2[0] == '-')
+	{
             sign = -1;
             str = ADD_INT(str1.erase(0, 1), str2.erase(0, 1));
-        } else {
+        } 
+        else 
+        {
             str = SUB_INT(str2, str1.erase(0, 1));
         }
-    } else {
+    } 
+    else 
+    {
         if(str2[0] == '-')
             str = SUB_INT(str1, str2.erase(0, 1));
-        else {
+        else
+        {
             string::size_type l1, l2;
             int i;
             l1 = str1.size(); l2 = str2.size();
-            if(l1 < l2) {
+            if(l1 < l2)
+            {
                 for(i = 1; i <= l2 - l1; i++)
                     str1 = "0" + str1;
-            } else {
+            } 
+            else
+            {
                 for(i = 1; i <= l1 - l2; i++)
                     str2 = "0" + str2;
             }
             int int1 = 0, int2 = 0; 
-            for(i = str1.size() - 1; i >= 0; i--) {
+            for(i = str1.size() - 1; i >= 0; i--) 
+            {
                 int1 = (int(str1[i]) - '0' + int(str2[i]) - '0' + int2) % 10;
                 int2 = (int(str1[i]) - '0' + int(str2[i]) - '0' +int2) / 10;
                 str = char(int1 + '0') + str;
@@ -68,58 +81,76 @@ string ADD_INT(string str1, string str2) {
         str = "-" + str;
     return str;
 }
-string SUB_INT(string str1, string str2) {
+string SUB_INT(string str1, string str2)
+{
     string MUL_INT(string str1, string str2);
     int sign = 1;
     string str;
     int i;
     if(str2[0] == '-')
         str = ADD_INT(str1, str2.erase(0, 1));
-    else {
-        int res = compare(str1, str2);
-        if(res == 0) return "0";
-        if(res < 0) {
-            sign = -1;
-            string temp = str1;
-            str1 = str2;
-            str2 = temp;
-        }
-        string::size_type tempint;
-        tempint = str1.size() - str2.size();
-        for(i = str2.size() - 1; i >= 0; i--) {
-            if(str1[i + tempint] < str2[i]) {
-                str1[i + tempint - 1] = char(int(str1[i + tempint - 1]) - 1);
-                str = char(str1[i + tempint] - str2[i] + ':') + str;
-            } else
-                str = char(str1[i + tempint] - str2[i] + '0') + str;
-        }
-        for(i = tempint - 1; i >= 0; i--)
-            str = str1[i] + str;
-    }
-    str.erase(0, str.find_first_not_of('0'));
-    if(str.empty()) str = "0";
-    if((sign == -1) && (str[0] != '0'))
-        str = "-" + str;
+    else 
+    {
+	if(str1[0] == '-')
+	{
+		str = "-" + ADD_INT(str1.erase(0,1),str2);
+	}
+	else
+	{
+       	 	int res = compare(str1, str2);
+        	if(res == 0) return "0";
+        	if(res < 0) 
+        	{
+          	  sign = -1;
+          	  string temp = str1;
+          	  str1 = str2;
+          	  str2 = temp;
+      		}
+       		string::size_type tempint;
+       		tempint = str1.size() - str2.size();
+       	 	for(i = str2.size() - 1; i >= 0; i--)
+        	{
+            		if(str1[i + tempint] < str2[i])
+           		{
+               			str1[i + tempint - 1] = char(int(str1[i + tempint - 1]) - 1);
+               			str = char(str1[i + tempint] - str2[i] + ':') + str;
+           		}
+			else
+               			 str = char(str1[i + tempint] - str2[i] + '0') + str;
+       		 }
+        	for(i = tempint - 1; i >= 0; i--)
+            	str = str1[i] + str;
+   	 	}		
+    		str.erase(0, str.find_first_not_of('0'));
+   		if(str.empty()) str = "0";
+   		if((sign == -1) && (str[0] != '0'))
+        	str = "-" + str;
+	}
     return str;
 }
-string MUL_INT(string str1, string str2) {
+string MUL_INT(string str1, string str2)
+{
     int sign = 1;
     string str;
-    if(str1[0] == '-') {
+    if(str1[0] == '-')
+    {
         sign *= -1;
         str1 = str1.erase(0, 1);
     }
-    if(str2[0] == '-') {
+    if(str2[0] == '-') 
+    {
         sign *= -1;
         str2 = str2.erase(0, 1);
     }
     int i, j;
     string::size_type l1, l2;
     l1 = str1.size(); l2 = str2.size();
-    for(i = l2 - 1; i >= 0; i --) {
+    for(i = l2 - 1; i >= 0; i --)
+    {
         string tempstr;
         int int1 = 0, int2 = 0, int3 = int(str2[i]) - '0';
-        if(int3 != 0) {
+        if(int3 != 0) 
+        {
             for(j = 1; j <= (int)(l2 - 1 - i); j++)
                 tempstr = "0" + tempstr;
             for(j = l1 - 1; j >= 0; j--) {
@@ -137,46 +168,61 @@ string MUL_INT(string str1, string str2) {
         str = "-" + str;
     return str;
 }
-string DIVIDE_INT(string str1, string str2, int flag) {
+string DIVIDE_INT(string str1, string str2, int flag)
+{
     string quotient, residue; 
     int sign1 = 1, sign2 = 1;
-    if(str2 == "0") {
+    if(str2 == "0") 
+    {
         quotient = "ERROR!";
         residue = "ERROR!";
         if(flag == 1) return quotient;
         else return residue;
     }
-    if(str1 == "0") {
+    if(str1 == "0")
+    {
         quotient = "0";
         residue = "0";
     }
-    if(str1[0] == '-') {
+    if(str1[0] == '-') 
+    {
         str1 = str1.erase(0, 1);
         sign1 *= -1;
         sign2 = -1;
     }
-    if(str2[0] == '-') {
+    if(str2[0] == '-') 
+    {
         str2 = str2.erase(0, 1);
         sign1 *= -1;
     }
     int res = compare(str1, str2);
-    if(res < 0) {
+    if(res < 0) 
+    {
         quotient = "0";
         residue = str1;
-    } else if(res == 0) {
+    }
+    else if(res == 0)
+    {
         quotient = "1";
         residue = "0";
-    } else {
+    } 
+    else 
+    {
         string::size_type l1, l2;
         l1 = str1.size(); l2 = str2.size();
         string tempstr;
         tempstr.append(str1, 0, l2 - 1);
-        for(int i = l2 - 1; i < l1; i++) {
+        for(int i = l2 - 1; i < l1; i++)
+        {
             tempstr = tempstr + str1[i];
-            for(char ch = '9'; ch >= '0'; ch --) {
+	    tempstr.erase(0, tempstr.find_first_not_of('0'));
+            if(tempstr.empty()) tempstr = "0";
+            for(char ch = '9'; ch >= '0'; ch --)
+            {
                 string str;
                 str = str + ch;
-                if(compare(MUL_INT(str2, str), tempstr) <= 0) {
+                if(compare(MUL_INT(str2, str), tempstr) <= 0)
+                {
                     quotient = quotient + ch;
                     tempstr = SUB_INT(tempstr, MUL_INT(str2, str));
                     break;
@@ -194,13 +240,16 @@ string DIVIDE_INT(string str1, string str2, int flag) {
     if(flag == 1) return quotient;
     else return residue;
 }
-string DIV_INT(string str1, string str2) {
+string DIV_INT(string str1, string str2)
+{
     return DIVIDE_INT(str1, str2, 1);
 }
-string MOD_INT(string str1, string str2) {
+string MOD_INT(string str1, string str2)
+{
     return DIVIDE_INT(str1, str2, 0);
 }
-string solve(string str){
+string solve(string str)
+{
 	stack<string> S1;
 	stack<string> S2;
 	for(int i = 0;i < str.size();)
@@ -242,20 +291,20 @@ string solve(string str){
 			else
 			{
 				string temp = "";
-				temp += str[i];
-				if(getPriority(str[i]) >= getPriority(temp[0]))
+				temp  = S2.top();
+				if(getPriority(str[i]) > getPriority(temp[0]))
 				{
-				        temp = "";
+					temp = "";
 					temp += str[i];
 					S2.push(temp);
 					i++;
 				}
 				else 
 				{
-					temp = "";
-					temp += str[i];
+					temp  = S2.top();
 					do
 					{
+						temp  = S2.top();
 						S1.push(S2.top());
 						S2.pop();
 					}while(!S2.empty() &&  getPriority(str[i]) < getPriority(temp[0]));
@@ -299,7 +348,7 @@ string solve(string str){
 			}
 			if(temp == "-")
 			{
-				S1.push(SUB_INT(str1,str2));
+				S1.push(SUB_INT(str2,str1));
 			}
 			if(temp == "*")
 			{
@@ -307,21 +356,24 @@ string solve(string str){
 			}
 			if(temp == "/")
 			{
-				S1.push(DIVIDE_INT(str1,str2,0));
+				S1.push(DIVIDE_INT(str2,str1,1));
                         }	
 		}
 		
 	}
 	return S1.top();
 }
-string pretreatment(string str){
+string pretreatment(string str)
+{
 	if(str[0] == '-')
 		str.insert(0,"0");
-	for(int i = 1;i < str.size();i++){
+	for(int i = 1;i < str.size();i++)
+	{
 		if(str[i] == '-' && (str[i - 1] < '0' || str[i - 1] > '9') && str[i - 1] != ')')
 		{
 			str.insert(i,"0");
-		}}return str;
+		}
+	}return str;
 }	
 int main(int argc,char **argv)
 {
